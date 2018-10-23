a177323938104d0c4e833c5cd8188b81a7e65a54
Asher Matheson <ashrlmthsn@gmail.com>
2018-10-23 18:28:55 +1300

    Bug Fixes

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
+#!/usr/bin/env python2
+
 import img_parse
 import solve
 
 import sys
 import argparse
 
-maze_path = None
-parser = argparse.ArgumentParser(description="Solve a given maze")
-parser.add_argument('-m', help='Path of maze to solve')
+def parse():
+    maze_path = None
+    parser = argparse.ArgumentParser(description="Solve a given maze")
+    parser.add_argument('-m', help='Path of maze to solve')
 
-args = parser.parse_args(sys.argv[1:])
-if args.m:
-    maze_path = args.m
-else:
-    raise ValueError('Maze path required')
-
-maze = img_parse.parse(maze_path)
+    args = parser.parse_args(sys.argv[1:])
+    if args.m:
+        maze_path = args.m
+    else:
+        raise ValueError('Maze path required')
