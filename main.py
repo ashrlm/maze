80313f99c46bb31b522b76bbac2cee5729bd5c92
Asher <ashrlmthsn@gmail.com>
2018-10-25 21:45:13 +1300

    Arg parsing error handling

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ parser = argparse.ArgumentParser(description="Solve a given maze")
 parser.add_argument('-m', help='Path of maze to solve')
 
 args = parser.parse_args(sys.argv[1:])
-maze_path = args.m
+if args.m:
+    maze_path = args.m
+else:
+    raise ValueError('Maze path required')
 
 maze = img_parse.parse(maze_path)
