7fe119e8534dfb1579408387d2d30bd6c0ee7afb
Asher <ashrlmthsn@gmail.com>
2018-10-25 23:29:48 +1300

    Docs

--- solve.py
+++ solve.py
@@ -6,9 +6,9 @@ def random_move(graph):
     conns = []
 
     while curr_node != graph.end:
-        conns.append(random.choice(curr_node.connections))
-        if conns[-1].nodes[0] == curr_node:
-            curr_node = conns[-1].nodes[1]
+        conns.append(random.choice(curr_node.connections)) #Move randomly
+        if conns[-1].nodes[0] == curr_node: #Check which one index of the conn our node is at
+            curr_node = conns[-1].nodes[1] #Use different node - Movement
 
         elif conns[-1].nodes[1] == curr_node:
             curr_node = conns[-1].nodes[0]
