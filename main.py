88097c414ed074c1aeb7380ef1a7db2fdcb41e55
Asher Matheson <ashrlmthsn@gmail.com>
2018-10-27 18:58:23 +1300

    Updated Parsing

--- main.py
+++ main.py
@@ -6,13 +6,28 @@ import solve
 import sys
 import argparse
 
-def parse():
+def parse_args():
     maze_path = None
     parser = argparse.ArgumentParser(description="Solve a given maze")
     parser.add_argument('-m', help='Path of maze to solve')
+    parser.add_argument('-a', help="Algorithm to use for solving")
+    parser.add_argument('-d', help='Directions if using dir_pri mode')
+
+    parsed = {}
 
     args = parser.parse_args(sys.argv[1:])
     if args.m:
-        maze_path = args.m
+        parsed['m'] = args.m
     else:
         raise ValueError('Maze path required')
+    if args.a:
+        parsed['a'] = args.a
+        if args.a == 'dir_pri':
+            if args.d:
+                parsed['d'] = args.d
+            else:
+                parsed['d'] = 'dlur'
+    else:
+        raise ValueError('Algorithm Required')
+
+    return parsed
