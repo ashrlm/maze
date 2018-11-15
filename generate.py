7ee9c3be156f431d9b95300cf62f798249bf9620
Asher Matheson <ashrlmthsn@gmail.com>
2018-11-15 21:43:22 +1300

    Added XCONN Generation - Still broken (no YCONN)

--- generate.py
+++ generate.py
@@ -4,31 +4,17 @@ import math
 
 class Generate():
 
-    def check_close(a, lst, abs_tol):
+    def gen_hor(img, y, sx, ex):
 
-        for i in lst:
-            if math.isclose(a, i, abs_tol=abs_tol):
-                return True
+        if sx > ex:
+            sx, ex = ex, sx
 
-        return False
-
-    def gen_hor(size, ys, img):
-
-        y = random.randint(1, size-2)
-
-        while Generate.check_close(y, ys, 1):
-            y=random.randint(1, size-2)
-
-        hor_s_x = random.randint(1, size-3)
-        hor_e_x = random.randint(1+hor_s_x, size-2)
-
-        for x in range(hor_s_x, hor_e_x):
+        for x in range(sx, ex):
             img.putpixel((x, y), (255, 255, 255))
 
-        return y
-
-    def gen_vert(size, xs, img):
-        pass
+    def gen_ver(img, x, sy, ey):
+        for y in range(sy, ey):
+            img.putpixel((x, y), (255, 255, 255))
 
     def generate(size):
         img = Image.new('RGB', [size] * 2)
@@ -39,15 +25,28 @@ class Generate():
         img.putpixel((e_pos, size-1), (255, 255, 255))
         img.putpixel((e_pos, size-2), (255, 255, 255))
 
-        xs = []
-        ys = []
-        curr_y = size - 2 #-2 to ensure one up
-        while curr_y != 0: #generate conns
-            if random.random() > .5:
-                ys.append(Generate.gen_hor(size, ys, img))
+        x = e_pos
+        y = size - 2
+
+        while y != 0:
+            if random.random() > .5 or x in (1, size-2): #XCONN - Ensure not on border
+                dist = random.randint(2, size-3)
+                s_x = random.randint(min(x, abs(x-dist)), x) #BUG - Check not going into negative
+                if s_x - dist > 0:
+                    e_x = s_x - dist
+
+                elif s_x + dist < size:
+                    e_x = s_x + dist
+
+                else:
+                    continue
+
+                Generate.gen_hor(img, y, s_x, e_x)
+                x = e_x
 
             else:
-                xs.append(Generate.gen_vert(size, xs, img))
+                pass # TODO: YCONN
+
 
         img.save('imgs/' + str(size) * 2 + '.png')
         img.show()
